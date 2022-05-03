979d0884d85ea97e7b894efea2bf90da1f73914a
fatemekholousi <fatemekholousizaer@gmail.com>
2022-05-03 23:54:17 +0430

    fix: product api ulploading is in progress

--- routes/customers.js
+++ routes/customers.js
@@ -1,4 +1,4 @@
-const {Customer,validate:validateCustomers}=require("./models/customer")
+const { Customer, validate: validateCustomers } = require("./models/customer");
 const express = require("express");
 
 const router = express.Router();
@@ -27,27 +27,25 @@ router.post("/", async (req, res) => {
 router.put("/:_id", async (req, res) => {
   const { error } = validateCustomers(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  const customer = await Customer.findByIdAndUpdate(
-    req.params._id,{  name: req.body.name,
-        phone: req.body.phone,
-        isGold: req.body.isGold,}
-  );
+  const customer = await Customer.findByIdAndUpdate(req.params._id, {
+    name: req.body.name,
+    phone: req.body.phone,
+    isGold: req.body.isGold,
+  });
   if (!customer) return res.status(404).send("You Poor child look lost");
   res.send(customer);
 });
 
-router.delete("/:_id", async(req, res) => {
-    const customer=await Customer.findByIdAndRemove(req.params._id)
-    if (!customer) return res.status(404).send("You Poor child look lost");
-    res.send(customer);
-  });
-  
-  router.get("/:_id", async(req, res) => {
-    const customer=await Customer.findById(req.params._id)
-    if (!customer) return res.status(404).send("You Poor child look lost");
-    res.send(customer);
-  });
+router.delete("/:_id", async (req, res) => {
+  const customer = await Customer.findByIdAndRemove(req.params._id);
+  if (!customer) return res.status(404).send("You Poor child look lost");
+  res.send(customer);
+});
 
-
+router.get("/:_id", async (req, res) => {
+  const customer = await Customer.findById(req.params._id);
+  if (!customer) return res.status(404).send("You Poor child look lost");
+  res.send(customer);
+});
 
 module.exports = router;
---
+++ routes/products.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Math.random().toString() + file.originalname)
+  },
+})
+
+const fileFilter = (req, file, cb) => {
+  //reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png')
+    cb(null, true)
+  else cb(null, false)
+}
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+  fileFilter: fileFilter,
+})
+
+const { Product, validate: validateProduct } = require('./models/product')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+  const products = await Product.find().sort('name')
+  res.send(products)
+})
+
+//don't forget json type in postman
+router.post('/', upload.single('productImage'), async (req, res) => {
+  const { error } = validateProduct(req.body)
+  if (error) {
+    return res.status(400).send(error.details[0].message)
+  }
+  console.log(req.file)
+  let product = new Product({
+    name: req.body.name,
+    genre: req.body.genre,
+    author: req.body.author,
+    summary: req.body.summary,
+    cover: req.body.cover,
+    stock: req.body.stock,
+    publishedYear: req.body.publishedYear,
+  })
+  product = await product.save()
+  res.send(product)
+})
+
+router.put('/:id', async (req, res) => {
+  const { error } = validateProduct(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+  const product = await Product.findByIdAndUpdate(req.params._id, {
+    name: req.body.name,
+    genre: req.body.genre,
+    author: req.body.author,
+    summary: req.body.summary,
+    cover: req.body.cover,
+    stock: req.body.stock,
+    publishedYear: req.body.publishedYear,
+  })
+  if (!product) return res.status(404).send('this is a 404, not found')
+  res.send(product)
+})
+
+router.delete('/:id', async (req, res) => {
+  const product = await Product.findByIdAndRemove(req.params._id)
+  if (!product) return res.status(404).send('this is a 404, not found')
+  res.send(product)
+})
+
+router.get('/:id', async (req, res) => {
+  const product = await Product.findById(req.params._id)
+  if (!product) return res.status(404).send('this is a 404, not found')
+  res.send(product)
+})
+
+module.exports = router
